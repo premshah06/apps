60a6b2d90d29a5d7396697d4840dfb170973d485
Prem Shah <102175127+premshah06@users.noreply.github.com>
2023-08-31 11:02:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 
     if selected == "Home":
         st.title("Diabetes and Heart Detection Website")
-        st.image("path_to_your_image.png", use_column_width=True)  # Add an attractive image
+        st.image("heart.jpg", use_column_width=True)  # Add an attractive image
         st.write("""Welcome to our Diabetes and Heart Prediction Website! In today's world, the prevalence of these conditions has risen significantly, leading to unfortunate outcomes for many individuals. Our mission is to tackle this challenge head-on by empowering you with knowledge about your health.""")
         st.write("""We understand that these diseases can be concerning, but worry not. Our user-friendly platform is designed to provide you with a reliable prediction of your health status based on the symptoms you might be experiencing. Instead of wondering and waiting, you can now take a proactive step towards your well-being from the comfort of your home.""")
         st.markdown(
