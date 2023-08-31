38c357b3cb0856e31a0c5ff87028688ad1d303fd
Prem Shah <88485022+prem0602@users.noreply.github.com>
2023-09-01 00:31:45 +0530

    1

--- heart_info.py
+++ heart_info.py
@@ -20,7 +20,7 @@ st.dataframe(heart_data.head())
 
 # Visualize the distribution of heart failure cases
 st.subheader("Heart Failure Cases Distribution")
-heart_failure_counts = heart_data["DEATH_EVENT"].value_counts()
+heart_failure_counts = heart_data["HeartDisease"].value_counts()
 st.bar_chart(heart_failure_counts)
 
 # Provide information about heart problems
