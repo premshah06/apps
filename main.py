0080baf552e3cd62c1f49c91b83b6b3145cb05b9
Prem Shah <102175127+premshah06@users.noreply.github.com>
2023-10-09 17:54:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ st.set_page_config(
 
 def main():
     with st.sidebar:
-        selected = option_menu("Main Menu", ["Home","Heart Problems","Heart Disease","Diabetes"],
-            icons=['house','activity','heart','person'], menu_icon="heart", default_index=0)
+        selected = option_menu("Main Menu", ["Home","Diabetes","Heart Problems","Heart Disease"],
+            icons=['house','person','activity','heart'], menu_icon="heart", default_index=0)
 
     if selected == "Home":
         st.markdown("""
